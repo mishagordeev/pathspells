3f2a97967c6e7e7d796ceeb6982830167b01b225
Mikhail Gordeev <mishagordeev@gmail.com>
2019-06-06 23:10:10 +0400

    - stateful widgets;

--- lib/main.dart
+++ lib/main.dart
@@ -4,130 +4,84 @@ import 'dart:convert';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-      ),
+      title: 'Welcome to Flutter',
       home: Scaffold(
-          appBar: AppBar(
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(
-                  Icons.search,
-                  semanticLabel: 'search',
-                ),
-                onPressed: () {
-                  print('Search button');
-                },
-              ),
-              IconButton(
-                icon: Icon(
-                  Icons.tune,
-                  semanticLabel: 'filter',
-                ),
-                onPressed: () {
-                  print('Filter button');
-                },
-              ),
-            ],
-          ),
-          body: new Container(
-            child: new Center(
-              // Use future builder and DefaultAssetBundle to load the local JSON file
-              child: new FutureBuilder(
-                  future: DefaultAssetBundle.of(context)
-                      .loadString('assets/spells.json'),
-                  builder: (context, snapshot) {
-                    List<Spell> spells =
-                        parseJosn(snapshot.data.toString());
-                    return !spells.isEmpty
-                        ? new SpellList(spell: spells)
-                        : new Center(child: new CircularProgressIndicator());
-                  }),
-            ),
-          )),
+        appBar: AppBar(
+          title: SearchBar('Misha'),
+        ),
+        body: Center(
+          child: StateFul(1),
+        ),
+      ),
     );
   }
 }
 
-List<Spell> parseJosn(String response) {
-  if (response == null) {
-    return [];
+class SearchBatState extends State<SearchBar> {
+  String _govno1;
+  SearchBatState(String g) {
+    print(g);
   }
-  final parsed = json.decode(response.toString()).cast<Map<String, dynamic>>();
-  return parsed.map<Spell>((json) => new Spell.fromJson(json)).toList();
+  @override
+  Widget build(BuildContext context) {
+    String _test;
+    SearchBatState("dfgdfg");
+    return Text(
+      'Hello, $_govno1 How are you?',
+      textAlign: TextAlign.center,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(fontWeight: FontWeight.bold),
+    );
+  }
+
+  int Test() {
+    print('sddsf');
+  }
 }
 
-class Spell {
-  final String name;
-  final String description;
+class SearchBar extends StatefulWidget {
+  String _govno;
 
-  Spell({this.name, this.description});
-
-  factory Spell.fromJson(Map<String, dynamic> json) {
-    return new Spell(
-        name: json['name'] as String, description: json['description'] as String);
+  SearchBar(String t) {
+    _govno = t;
+    print('govno' + _govno + ' ');
+    print('t' + t + ' ');
+  }
+  @override
+  SearchBatState createState() {
+    return SearchBatState(_govno);
   }
 }
 
-class SpellList extends StatelessWidget {
-  final List<Spell> spell;
-  SpellList({Key key, this.spell}) : super(key: key);
+
+class RestStateFul extends State<StateFul> {
+  RestStateFul(int i);
 
   @override
   Widget build(BuildContext context) {
-    return new ListView.builder(
-        itemCount: spell == null ? 0 : spell.length,
-        itemBuilder: (BuildContext context, int index) {
-          return new ListTile(
-              title: Text(spell[index].name),
-              subtitle: Text(spell[index].description),
-              onTap: () {
-                _printTest(spell[index],context);
-                /*Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SecondRoute()),
-                );*/
-              });
-        });
-  }
-
-  void _printTest(Spell test, BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        // Add 20 lines from here...
-        builder: (BuildContext context) {
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(test.name),
-            ),
-            body: Text(test.description),
-          );
-        },
-      ),
+    final List<String> entries = <String>['A', 'B', 'C'];
+    final List<int> colorCodes = <int>[600, 500, 100];
+    return ListView.separated(
+      padding: const EdgeInsets.all(8.0),
+      itemCount: entries.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 50,
+          color: Colors.amber[colorCodes[index]],
+          child: Center(child: Text('Entry ${entries[index]}')),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
     );
   }
 }
 
-class SecondRoute extends StatelessWidget {
+class StateFul extends StatefulWidget {
+  StateFul(int i);
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Route"),
-      ),
-      body: Center(
-        //child: RaisedButton(
-        ////  onPressed: () {
-        //    // Navigate back to first route when tapped.
-        //  },
-        //  child: Text('Go back!'),
-        //),
-      ),
-    );
-  }
+  RestStateFul createState() => RestStateFul(123);
 }
