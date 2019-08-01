2dfd4c551193ac215f8103a7d1a0f4f55d02444e
Mikhail Gordeev <m.gordeev@kefirgames.com>
2019-08-01 19:12:12 +0400

    - add: has not zero level attribute;

--- lib/Class.dart
+++ lib/Class.dart
@@ -3,14 +3,16 @@ class Class {
   final String name;
   final String image;
   final int levelCount;
+  final bool hasNotZeroLevel;
 
-  Class({this.id, this.name, this.image, this.levelCount});
+  Class({this.id, this.name, this.image, this.levelCount, this.hasNotZeroLevel});
 
   factory Class.get(Map<String, dynamic> json) {
     return new Class(
         id: json['id'] as String,
         name: json['name'] as String,
         image: json['image'] as String,
-        levelCount: json['level_count'] as int);
+        levelCount: json['level_count'] as int,
+        hasNotZeroLevel: json['has_not_zero_level'] as bool);
   }
 }
